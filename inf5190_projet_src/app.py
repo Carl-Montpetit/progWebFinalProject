b4ce27cc6a12dc013bd5a8201f8b0e970084f3bb
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2021-12-10 11:10:58 -0500

    newFeature(app.py):✨backgrounds scheduler🤌🏻

--- inf5190_projet_src/app.py
+++ inf5190_projet_src/app.py
@@ -2,9 +2,13 @@
 # LIBRARY #
 ################################################################################
 from db.db import Database
-from datetime import time
+from datetime import datetime
 from flask_bootstrap import Bootstrap
-from flask import render_template, redirect, request, url_for, flash, Flask, jsonify, make_response, abort, session, get_flashed_messages
+from flask import render_template, redirect, request, url_for, flash, Flask, \
+    jsonify, make_response, abort, session, get_flashed_messages
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+import pytz
 
 ################################################################################
 # CONSTANTS #
@@ -12,22 +16,45 @@ from flask import render_template, redirect, request, url_for, flash, Flask, jso
 ERROR_TEMPLATE = 'error.html'
 
 ################################################################################
-# INITIALIZE FLASK APPLICATION
+# INITIALIZE FLASK APPLICATION #
 ################################################################################
+local_time_quebec_canada = datetime.now(pytz.timezone('America/Toronto'))
 # Create and initialize a Flask application called app
 app = Flask(
     __name__, static_folder="static", template_folder="templates",
     static_url_path=""
 )
 
-
 app.config['SECRET_KEY'] = 'hard_to_guess_string'
 
 # link flask-bootstrap with the application
 bootstrap = Bootstrap(app)
 
-# Create a database object called db
+# create a database object called db
 db = Database()
+
+# link scheduler with the application
+scheduler = BackgroundScheduler(daemon=True)
+
+
+################################################################################
+# SCHEDULER #
+################################################################################
+
+def scheduled_database_update():
+    db.create_piscines_installations_aquatiques_table()
+    db.add_piscines_installations_aquatiques_data_to_database()
+    # db.create_glissades_table()
+    # db.add_glissades_data_to_database()
+    # db.create_patinoires_table()
+    # db.add_patinoires_data_to_database()
+    print('The database was updated at {}'.format(local_time_quebec_canada))
+
+
+# Update database every day at midnight
+scheduler.add_job(scheduled_database_update, 'cron', hour='0', day='*')
+scheduler.start()
+
 
 ################################################################################
 # ERROR HANDLERS #
@@ -68,26 +95,19 @@ def not_implemented_yet(error):
 def service_unavailable(error):
     return render_template(ERROR_TEMPLATE, error=error), 503
 
+
 ################################################################################
-# SERVICES #
+# REST SERVICES #
 ################################################################################
 
 
 @app.route("/")
 def index():
-    # db.create_piscines_installations_aquatiques_table()
-    # db.add_piscines_installations_aquatiques_data_to_database()
-    # db.create_glissades_table()
-    # db.add_glissades_data_to_database()
-    db.create_patinoires_table()
-    db.add_patinoires_data_to_database()
+    """main route of application"""
+    print(local_time_quebec_canada)
     return render_template("home.html")
 
 
-################################################################################
-# close connection to database #
-################################################################################
-# db.disconnect()
 ################################################################################
 # This is always true if app.py is used as entry point of the interpreter #
 ################################################################################
