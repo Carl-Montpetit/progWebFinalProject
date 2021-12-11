a765124a611206ed0491123f06ca55abc2ca0a2f
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2021-12-11 07:20:05 -0500

    refactor, bugFix(db.py):🎨🐛Optimized the code and cleaned it

--- inf5190_projet_src/db/db.py
+++ inf5190_projet_src/db/db.py
@@ -6,13 +6,32 @@ import xml.etree.ElementTree as elementTree
 ################################################################################
 # CONSTANTS
 ################################################################################
-URL_CSV_PISCINES_INSTALLATIONS_AQUATIQUES = "https://data.montreal.ca/dataset/4604afb7-a7c4-4626-a3ca-e136158133f2/resource/cbdca706-569e-4b4a-805d-9af73af03b14/download/piscines.csv"
-INSERT_PISCINES_INSTALLATIONS_AQUATIQUES = "INSERT INTO piscines_installations_aquatiques (id_uev, type, nom, arrondissement, adresse, propriete, gestion, point_x, point_y, equipement, longitude, latitude) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
-DROP_PISCINES_INSTALLATIONS_AQUATIQUES = "DROP TABLE IF EXISTS piscines_installations_aquatiques;"
-DELETE_TITLES_ROW_PISCINES_INSTALLATIONS_AQUATIQUES = "DELETE FROM " \
-                                                     "piscines_installations_aquatiques WHERE id=1"
-CREATE_PISCINES_INSTALLATIONS_AQUATIQUES = "CREATE TABLE piscines_installations_aquatiques(id INTEGER PRIMARY KEY AUTOINCREMENT, id_uev INTEGER, type VARCHAR(100), nom VARCHAR(100), arrondissement VARCHAR(100), adresse VARCHAR(100), propriete VARCHAR(100), gestion VARCHAR(100), point_x INTEGER, point_y INTEGER, equipement VARCHAR(100), longitude INTEGER, latitude INTEGER);"
-URL_XML_PATINOIRES = "https://data.montreal.ca/dataset/225ac315-49fe-476f-95bd-a1ce1648a98c/resource/5d1859cc-2060-4def-903f-db24408bacd0/download/l29-patinoire.xml"
+URL_CSV_PISCINES = "https://data.montreal.ca/dataset/4604afb7-a7c4-4626-a3ca-e136158133f2/resource/cbdca706-569e-4b4a-805d-9af73af03b14/download/piscines.csv"
+INSERT_PISCINES = "INSERT INTO piscines_installations_aquatiques (id_uev, " \
+                  "type,  nom, arrondissement, adresse, propriete, gestion,  " \
+                  "point_x, point_y, equipement, longitude, latitude) VALUES " \
+                  "(?,  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+DROP_PISCINES = "DROP TABLE IF EXISTS piscines_installations_aquatiques;"
+DESTROY_NBSP_PISCINES = "UPDATE piscines_installations_aquatiques SET adresse " \
+                        "= REPLACE(adresse,' ', ' ');"
+DELETE_TITLES_PISCINES = "DELETE FROM piscines_installations_aquatiques WHERE " \
+                         "id=1"
+EMPTY_IS_NULL_ADRESSE = "UPDATE piscines_installations_aquatiques SET adresse = NULLIF(adresse, '');"
+EMPTY_IS_NULL_PROPRIETE = "UPDATE piscines_installations_aquatiques SET " \
+                          "propriete = NULLIF(propriete, '');"
+EMPTY_IS_NULL_GESTION = "UPDATE piscines_installations_aquatiques SET gestion = NULLIF(gestion, '');"
+EMPTY_IS_NULL_EQUIPEMENT = "UPDATE piscines_installations_aquatiques SET " \
+                           "equipement = NULLIF(equipement, '');"
+CREATE_PISCINES = "CREATE TABLE " \
+                  "piscines_installations_aquatiques(id INTEGER PRIMARY KEY  " \
+                  "AUTOINCREMENT, id_uev INTEGER, type VARCHAR(100), " \
+                  "nom  VARCHAR(100), arrondissement VARCHAR(100), adresse  " \
+                  "VARCHAR(100), propriete VARCHAR(100), gestion VARCHAR(" \
+                  "100),  point_x INTEGER, point_y INTEGER, equipement  " \
+                  "VARCHAR(100), longitude INTEGER, latitude INTEGER);"
+URL_XML_PATINOIRES = "https://data.montreal.ca/dataset/225ac315-49fe-476f" \
+                     "-95bd-a1ce1648a98c/resource/5d1859cc-2060-4def-903f-db" \
+                     "24408bacd0/download/l29-patinoire.xml"
 INSERT_PATINOIRES = "INSERT INTO patinoires (nom_arr, nom_pat, date_heure, " \
                     "ouvert, deblaye, arrose, resurface) VALUES (?, ?, " \
                     "?, ?, ?, ?, ?);"
@@ -22,7 +41,23 @@ CREATE_PATINOIRES = "CREATE TABLE patinoires(id INTEGER PRIMARY KEY " \
                     "100), date_heure TEXT," \
                     "ouvert NUMERIC, deblaye NUMERIC, arrose NUMERIC," \
                     "resurface NUMERIC);"
-URL_XML_GLISSADES = "http://www2.ville.montreal.qc.ca/services_citoyens/pdf_transfert/L29_GLISSADE.xml"
+EMPTY_IS_NULL_OUVERT_PATINOIRES = "UPDATE patinoires SET ouvert = NULLIF(" \
+                                  "ouvert, " \
+                                  "'None');"
+EMPTY_IS_NULL_DEBLAYE_PATINOIRES = "UPDATE patinoires SET deblaye = NULLIF(" \
+                                   "deblaye, " \
+                                   "'None');"
+EMPTY_IS_NULL_ARROSE_PATINOIRES = "UPDATE patinoires SET arrose = NULLIF(" \
+                                  "arrose, " \
+                                  "'None');"
+EMPTY_IS_NULL_RESURFACE_PATINOIRES = "UPDATE patinoires SET resurface = " \
+                                     "NULLIF(" \
+                                     "resurface, " \
+                                     "'None');"
+DELETE_UGLY_PARENTHESIS = "UPDATE patinoires SET nom_pat = REPLACE(nom_pat," \
+                          "'()', '')"
+URL_XML_GLISSADES = "http://www2.ville.montreal.qc.ca/services_citoyens" \
+                    "/pdf_transfert/L29_GLISSADE.xml"
 INSERT_GLISSADES = "INSERT INTO glissades (nom, nom_arr, cle, " \
                    "date_maj, ouvert, deblaye, condition) VALUES (?, ?, " \
                    "?, ?, ?, ?, ?);"
@@ -32,11 +67,19 @@ CREATE_GLISSADES = "CREATE TABLE glissades(id INTEGER PRIMARY KEY " \
                    "100), cle VARCHAR(100), date_maj TEXT," \
                    "ouvert NUMERIC, deblaye NUMERIC, " \
                    "condition VARCHAR(100));"
+EMPTY_IS_NULL_OUVERT_GLISSADES = "UPDATE glissades SET ouvert = NULLIF(" \
+                                 "ouvert, " \
+                                 "'None');"
+EMPTY_IS_NULL_DEBLAYE_GLISSADES = "UPDATE glissades SET deblaye = NULLIF(" \
+                                  "deblaye, " \
+                                  "'None');"
 
 
 ################################################################################
 # STATIC FUNCTIONS
 ################################################################################
+
+
 def get_csv_data_from_url(url):
     """Get the piscines data"""
     with requests.Session() as s:
@@ -69,9 +112,68 @@ def print_xml_tree(root):
     return 1
 
 
+def multiple_query_patinoires(cursor):
+    cursor.execute(EMPTY_IS_NULL_OUVERT_PATINOIRES)
+    cursor.execute(EMPTY_IS_NULL_DEBLAYE_PATINOIRES)
+    cursor.execute(EMPTY_IS_NULL_ARROSE_PATINOIRES)
+    cursor.execute(EMPTY_IS_NULL_RESURFACE_PATINOIRES)
+    cursor.execute(DELETE_UGLY_PARENTHESIS)
+
+
+def multiple_query_piscines(cursor, rows):
+    cursor.executemany(INSERT_PISCINES, rows)
+    cursor.execute(DELETE_TITLES_PISCINES)
+    cursor.execute(DESTROY_NBSP_PISCINES)
+    cursor.execute(EMPTY_IS_NULL_ADRESSE)
+    cursor.execute(EMPTY_IS_NULL_EQUIPEMENT)
+    cursor.execute(EMPTY_IS_NULL_GESTION)
+    cursor.execute(EMPTY_IS_NULL_PROPRIETE)
+
+
+def get_conditions_fields(conditions, j, k, noms_pats):
+    date_heure = conditions[k + j * int(len(
+        conditions) / len(noms_pats))][0].text.strip()
+    ouvert = conditions[k + j * int(len(
+        conditions) / len(noms_pats))][1].text.strip()
+    deblaye = conditions[k + j * int(len(
+        conditions) / len(noms_pats))][2].text.strip()
+    arrose = conditions[k + j * int(len(
+        conditions) / len(noms_pats))][3].text.strip()
+    resurface = conditions[k + j * int(len(
+        conditions) / len(noms_pats))][4].text.strip()
+    return arrose, date_heure, deblaye, ouvert, resurface
+
+
+def get_glissades_fields(glissade):
+    nom = glissade.find("nom")
+    nom.text = 'None' if nom.text is None else glissade.find(
+        "nom").text.strip()
+    nom_arr = glissade.find("arrondissement").find("nom_arr")
+    nom_arr.text = 'None' if nom_arr.text is None else \
+        glissade.find("arrondissement").find("nom_arr").text.strip()
+    cle = glissade.find("arrondissement").find("cle")
+    cle.text = 'None' if cle.text is None else glissade.find(
+        "arrondissement").find("cle").text.strip()
+    date_maj = glissade.find("arrondissement").find("date_maj")
+    date_maj.text = 'None' if date_maj.text is None else \
+        glissade.find("arrondissement").find("date_maj").text.strip()
+    ouvert = glissade.find("ouvert")
+    ouvert.text = 'None' if ouvert.text is None else \
+        glissade.find("ouvert").text.strip()
+    deblaye = glissade.find("deblaye")
+    deblaye.text = 'None' if deblaye.text is None else \
+        glissade.find("deblaye").text.strip()
+    condition = glissade.find("condition")
+    condition.text = 'None' if condition.text is None else \
+        glissade.find("condition").text.strip()
+    return cle, condition, date_maj, deblaye, nom, nom_arr, ouvert
+
+
 ################################################################################
 # DATABASE OBJECT
 ################################################################################
+
+
 class Database:
     ############################################################################
     # CONSTRUCTOR
@@ -101,9 +203,8 @@ class Database:
         """add csv data from url to database"""
         connection = self.get_connection()
         cursor = connection.cursor()
-        rows = get_csv_data_from_url(URL_CSV_PISCINES_INSTALLATIONS_AQUATIQUES)
-        cursor.executemany(INSERT_PISCINES_INSTALLATIONS_AQUATIQUES, rows)
-        cursor.execute(DELETE_TITLES_ROW_PISCINES_INSTALLATIONS_AQUATIQUES)
+        rows = get_csv_data_from_url(URL_CSV_PISCINES)
+        multiple_query_piscines(cursor, rows)
         connection.commit()
         connection.close()
         self.disconnect()
@@ -122,19 +223,12 @@ class Database:
             for j, nom in enumerate(noms_pats):
                 nom_pat = nom.text.strip()
                 for k in range(int(len(conditions) / len(noms_pats))):
-                    date_heure = conditions[k + j * int(len(
-                        conditions) / len(noms_pats))][0].text.strip()
-                    ouvert = conditions[k + j * int(len(
-                        conditions) / len(noms_pats))][1].text.strip()
-                    deblaye = conditions[k + j * int(len(
-                        conditions) / len(noms_pats))][2].text.strip()
-                    arrose = conditions[k + j * int(len(
-                        conditions) / len(noms_pats))][3].text.strip()
-                    resurface = conditions[k + j * int(len(
-                        conditions) / len(noms_pats))][4].text.strip()
+                    arrose, date_heure, deblaye, ouvert, resurface = get_conditions_fields(
+                        conditions, j, k, noms_pats)
                     cursor.execute(INSERT_PATINOIRES, (
                         nom_arr, nom_pat, date_heure, ouvert, deblaye, arrose,
                         resurface))
+        multiple_query_patinoires(cursor)
         connection.commit()
         connection.close()
         self.disconnect()
@@ -144,20 +238,17 @@ class Database:
         connection = self.get_connection()
         cursor = connection.cursor()
         glissades = get_xml_data_from_url(URL_XML_GLISSADES)
-        glissades_data = []
+        final = []
         for glissade in glissades:
-            nom = glissade.find("nom").text.strip()
-            nom_arr = glissade.find("arrondissement").find(
-                "nom_arr").text.strip()
-            cle = glissade.find("arrondissement").find("cle").text.strip()
-            date_maj = glissade.find("arrondissement").find(
-                "date_maj").text.strip()
-            ouvert = glissade.find("ouvert").text
-            deblaye = glissade.find("deblaye").text.strip()
-            condition = glissade.find("condition").text.strip()
-            total = (nom, nom_arr, cle, date_maj, ouvert, deblaye, condition)
-            glissades_data.append(total)
-        cursor.executemany(INSERT_GLISSADES, glissades_data)
+            cle, condition, date_maj, deblaye, nom, nom_arr, ouvert = get_glissades_fields(
+                glissade)
+            total = (nom.text.strip(), nom_arr.text.strip(), cle.text.strip(),
+                     date_maj.text.strip(), ouvert.text.strip(),
+                     deblaye.text.strip(), condition.text.strip())
+            final.append(total)
+        cursor.executemany(INSERT_GLISSADES, final)
+        cursor.execute(EMPTY_IS_NULL_OUVERT_GLISSADES)
+        cursor.execute(EMPTY_IS_NULL_DEBLAYE_GLISSADES)
         connection.commit()
         connection.close()
         self.disconnect()
@@ -165,8 +256,8 @@ class Database:
     def create_piscines_installations_aquatiques_table(self):
         connection = self.get_connection()
         cursor = connection.cursor()
-        cursor.execute(DROP_PISCINES_INSTALLATIONS_AQUATIQUES)
-        cursor.execute(CREATE_PISCINES_INSTALLATIONS_AQUATIQUES)
+        cursor.execute(DROP_PISCINES)
+        cursor.execute(CREATE_PISCINES)
         connection.commit()
         connection.close()
         self.disconnect()
